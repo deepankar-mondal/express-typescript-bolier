a0c87a8b5c11af75f14d76d4858515314a539ec2
deepankar-tripfactory <deepankar.mondal@tripfactory.com>
2024-11-03 13:34:26 +0530

    initial commit

---
+++ src/helpers/error-handler.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+// write a single class for 404 and 500 error
+
+class ErrorHandler {
+  static notFound(req: Request, res: Response, next: NextFunction) {
+    res.status(404).json({ message: "Resource not found" });
+  }
+
+  static serverError(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+export default ErrorHandler;
